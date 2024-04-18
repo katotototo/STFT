287a2f6aa49e9ddb1a85c2e3d74f68a3777de129
halogawa <st20049@t.kagawa-nct.ac.jp>
2024-04-18 10:31:24 +0900

    step5
    関数にすることを試みる
    まだ実行はしていない

--- stft_step1.m
+++ stft_step1.m
@@ -1,11 +1,11 @@
-clear;close all;clc
+function S = f(SoundData,Fs,WinLen,ShiftWidth)
 
-[Signal,Fs] = audioread("bisei.wav");
+[Signal,Fs] = audioread("SoundData");
 %パラメータ設定
 % Fs = 16000; %サンプリング周波数
 % t = linspace(0,10,10*Fs);
 % f = 4000; %周波数
-n = 10;%窓長のパラメータ
+%n = 10;%窓長のパラメータ
 
 %信号の生成
 
@@ -14,8 +14,8 @@ n = 10;%窓長のパラメータ
 
 %各長決定
 %SignalLen = tMax*Fs; %信号長
-WinLen = power(2,n); %窓長
-ShiftWidth = WinLen/2; %シフト幅
+%WinLen = power(2,n); %窓長
+%ShiftWidth = WinLen/2; %シフト幅
 %J = ceil(SignalLen/ShiftWidth) + 1; %時間フレーム数
 
 %0を追加した波形を生成
@@ -26,6 +26,7 @@ Signal = [C,Signal.',D];
 SignalLen = size(Signal.',1);
 J = fix(SignalLen/ShiftWidth) - 1; %時間フレーム数
 
+A = zeros(WinLen,J); %ゼロ行列
 S = zeros(WinLen,J); %ゼロ行列
 HannWinWidth = (0:1:WinLen-1); %ハン
 HannWin = 0.5 - 0.5*cos((2*pi*HannWinWidth)/(WinLen-1));
@@ -36,18 +37,19 @@ for j = 1:J
     %temp = SinWave((j-1)*ShiftWidth + 1 :(j-1)*ShiftWidth + WinLen);
     temp = Signal((j-1)*ShiftWidth + 1 :(j-1)*ShiftWidth + WinLen);
     tempWin = HannWin.*temp;
-    temp1 = fft(tempWin);
+    S = fft(tempWin);
     temp2 = (abs(temp1));
     temp3 = 20*log10(temp2);
-    S(:,j) = temp3;
+    A(:,j) = temp3;
 end
 
 XAxis = linspace(0,SignalLen/Fs,J);
 YAxis = linspace(0,Fs,WinLen);
 axis xy
 
-imagesc(XAxis,YAxis,S);
+imagesc(XAxis,YAxis,A);
 axis xy
 xlabel("時間[s]");
 ylabel("周波数[Hz]");
 colorbar
+end
